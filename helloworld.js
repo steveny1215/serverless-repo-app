83511d6a89d06c2ccfb217298001ea0e41c8b02b
Steven Yeung <syeung10@binghamton.edu>
2022-06-09 20:14:23 -0400

    bruhhhhh

--- helloworld.js
+++ helloworld.js
@@ -1,5 +1,12 @@
 // code your `hello` function here!
+/*
+function hello(){
+    n = console.log("Hello World");
+    return n;
+}
+hello();
+*/
 
 module.exports = function hello(){
-    console.log('Hello World');
-}
+    console.log("Hello World");
+};
