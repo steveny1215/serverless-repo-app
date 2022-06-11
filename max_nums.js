178a3d74746f18bd75eeef0005cd605757b3a8b6
Steven Yeung <syeung10@binghamton.edu>
2022-06-11 17:23:37 -0400

    bro 1

--- max_nums.js
+++ max_nums.js
@@ -1,1 +1,13 @@
 // start coding your function here!
+
+function find_max(arr){
+    let max_num = 0;
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] >= max_num){
+            max_num = arr[i];
+        }
+    }
+    return max_num;
+}
+
+exports.find_max = find_max;
