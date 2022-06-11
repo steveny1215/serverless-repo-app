86a88716a6bdd92180157637461444920b3abc1c
Steven Yeung <syeung10@binghamton.edu>
2022-06-11 17:17:08 -0400

    bruh

--- late.js
+++ late.js
@@ -1,8 +1,9 @@
 // start coding your function here!
 function running_late(date){
-    if(date <= 10){
-        return "It's late!";
-    }else{
+    if(new Date(date).getHours() >= 22){
+        return "It is late!";
+    } 
+    else {
         return "It is still early!";
     }
 }
